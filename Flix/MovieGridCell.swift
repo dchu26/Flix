d257714a69b5a82da9f17ee8bc4ad3ce32a185ed
David Chu <dchu26@ucmerced.edu>
2021-09-13 14:06:32 -0700

    Updated Flix with Part 2 Requirements

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by David Chu on 9/13/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
